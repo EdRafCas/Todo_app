d1dcd88db603b3baef1f7b1835a8a2ee4a2fd1d3
EdRafCas <Eduardoo.rafael@gmail.com>
2023-02-08 23:56:22 -0300

    changes to css

--- src/App.js
+++ src/App.js
@@ -7,9 +7,22 @@ const App = () => {
       <div className='headerimageLight' >
         <img src={HeaderLight}alt='header daylight mode'/>
       </div>
-      {/* <div className='white-block'></div> */}
-      <div className='block'></div>
-      <div className='block'></div>
+      <div className='block'>
+        <div className='header-top'>
+          <span>TODO</span>
+        </div>
+        <div className='input-container'>
+          <div className='checkbox-container'>
+          0
+          </div>
+          <div className='input-container-2'>
+            currently typing
+          </div>
+        <div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
